8a55b4f98ef566dd6ef3cd843face04f6506ba58
Audrey Clark <amazon.audrey@gmail.com>
2024-08-26 19:36:36 -0500

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,104 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Philip J. Fry',
+    username: 'Fry',
+    email: 'philip.fry@planetexpress.com'
+  },
+  {
+    id: 2,
+    name: 'Turanga Leela',
+    username: 'Leela',
+    email: 'leela.turanga@planetexpress.com'
+  },
+  {
+    id: 3,
+    name: 'Bender Bending Rodríguez',
+    username: 'Bender',
+    email: 'bender@planetexpress.com'
+  },
+  {
+    id: 4,
+    name: 'Professor Farnsworth',
+    username: 'Professor',
+    email: 'hubert.farnsworth@planetexpress.com'
+  },
+  {
+    id: 5,
+    name: 'Dr. Zoidberg',
+    username: 'Zoidberg',
+    email: 'zoidberg@planetexpress.com'
+  },
+  {
+    id: 6,
+    name: 'Amy Wong',
+    username: 'Amy',
+    email: 'amy.wong@planetexpress.com'
+  },
+  {
+    id: 7,
+    name: 'Kif Kroker',
+    username: 'Kiffy',
+    email: 'kif.kroker@doop.gov'
+  },
+  {
+    id: 8,
+    name: 'Hermes Conrad',
+    username: 'Hermes',
+    email: 'hermes.conrad@planetexpress.com'
+  },
+  {
+    id: 9,
+    name: 'Zapp Brannigan',
+    username: 'Brannigan',
+    email: 'zapp.brannigan@doop.gov'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+  {
+    id: 10,
+    name: 'Scruffy',
+    username: 'Scruffy',
+    email: 'scruffy@planetexpress.com'
+  },
+];
